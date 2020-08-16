8abf4c1b231cf6b85c7da92aa7bbad4a17f123b7
Vanessa Lane <vlane@23andme.com>
2020-08-15 20:33:10 -1000

    move utils, setup add role to use new promises

--- utils/helpers.js
+++ utils/helpers.js
@@ -33,21 +33,5 @@ toTitleCase = (input) => {
     return inputArray.join(" ");
 }
 
-deleteById = (connection, table, id) => {
-    connection.query(
-        `DELETE FROM ${table} WHERE id = ${id}`,
-        function(err, res) {
-            if (err) {
-                console.log(`Couldn't delete record where id=${id} from ${table}. Error: ${err.sqlMessage}.`)
-                connection.end();
-                return false;
-            }
 
-            // let the user know if it worked and send them back to the main menu
-            console.log(`\nDeleted ${res.affectedRows} from ${table}.`);
-            showMenu(connection);
-        }
-    );
-};
-
-module.exports = {deleteById, validateRequired, validateNumber, toTitleCase};
+module.exports = {validateRequired, validateNumber, toTitleCase};
